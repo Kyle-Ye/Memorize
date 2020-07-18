5d9f4d6f5a0f3b2c032ceb52018cc06346050e9e
kyle <kyle201817146@gmail.com>
2020-07-18 23:55:13 +0800

    Change UI

--- Memorize/View/ContentView.swift
+++ Memorize/View/ContentView.swift
@@ -11,41 +11,55 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingAddTheme = false
     @State private var isRandomGame = false
+    @State private var editMode: EditMode = .inactive
 
     var body: some View {
         NavigationView {
-            ZStack(alignment: .bottomTrailing) {
-                List {
-                    ForEach(EmojiMemoryGame.themes, id: \.self) { theme in
-                        NavigationLink(destination: EmojiMemoryGameView()
-                            .environmentObject(EmojiMemoryGame(theme: theme))
-                            .onAppear {
-                                print("json = \(theme.json?.utf8 ?? "nil")")
-                            }
-                        ) {
-                            ThemeView(theme: theme)
+            List {
+                ForEach(EmojiMemoryGame.themes, id: \.self) { theme in
+                    NavigationLink(destination: EmojiMemoryGameView()
+                        .navigationBarTitleDisplayMode(.inline)
+                        .environmentObject(EmojiMemoryGame(theme: theme))
+                        .onAppear {
+                            print("json = \(theme.json?.utf8 ?? "nil")")
                         }
+                    ) {
+                        ThemeView(theme: theme, isEditing: editMode.isEditing)
                     }
                 }
-                .listStyle(PlainListStyle())
-                .background(
-                    Group {
-                        if isRandomGame {
-                            NavigationLink(destination: EmojiMemoryGameView().environmentObject(EmojiMemoryGame()), isActive: $isRandomGame, label: {
-                                EmptyView()
-                            })
-                        }
-                })
-                RoundButton(isOn: $showingAddTheme)
-                    .padding()
+                .onDelete { _ in
+//                    indexSet.map { store.documents[$0] }.forEach { document in
+//                        store.removeDocument(document)
+//                    }
+                }
+                Button("Random".getLocalized()) {
+                    isRandomGame = true
+                }
             }
-            .navigationBarTitle("Themes".getLocalized())
-            .navigationBarItems(trailing: Button("Random".getLocalized()) {
-                isRandomGame = true
+            .listStyle(PlainListStyle())
+            .background(
+                Group {
+                    if isRandomGame {
+                        NavigationLink(destination: EmojiMemoryGameView().environmentObject(EmojiMemoryGame()), isActive: $isRandomGame, label: {
+                            EmptyView()
+                        })
+                    }
             })
-        }
-        .sheet(isPresented: $showingAddTheme) {
-            NewThemeView()
+            .navigationBarTitle("Themes".getLocalized())
+            .navigationBarItems(
+                leading: Button(
+                    action: {
+//                        store.addDocument()
+                    }, label: {
+                        Image(systemName: "plus").imageScale(.large)
+                    }
+                ),
+                trailing: EditButton()
+            )
+            .environment(\.editMode, $editMode)
+            .sheet(isPresented: $showingAddTheme) {
+                NewThemeView()
+            }
         }
     }
 }
--- Memorize/View/ItemView/ThemeView.swift
+++ Memorize/View/ItemView/ThemeView.swift
@@ -10,19 +10,35 @@ import SwiftUI
 
 struct ThemeView: View {
     var theme: MemoryGame<String>.Theme
+    var isEditing: Bool = false
+
+    var description: String {
+        if theme.contents.count == theme.pairs {
+            return "All of"
+        } else {
+            return "\(theme.pairs) pairs from"
+        }
+    }
+
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(theme.name)
-                .bold()
-                .font(.title)
-                .foregroundColor(Color(theme.cardFaceDownColor))
-            Text(theme.contents.joined())
+        HStack {
+            if isEditing {
+                Image(systemName: "pencil.circle.fill").imageScale(.large).foregroundColor(Color(theme.cardFaceDownColor))
+            }
+            VStack(alignment: .leading) {
+                Text(theme.name)
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(Color(theme.cardFaceDownColor))
+                Text("\(description) \(theme.contents.joined())")
+                    .lineLimit(1)
+            }
         }
     }
 }
 
 struct ThemeView_Previews: PreviewProvider {
     static var previews: some View {
-        ThemeView(theme: EmojiMemoryGame.themes.randomElement()!)
+        ThemeView(theme: EmojiMemoryGame.themes.randomElement()!, isEditing: true)
     }
 }
